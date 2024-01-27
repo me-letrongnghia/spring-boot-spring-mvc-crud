d8e194b552096333106cc02af33b230299c6957f
letrongnghia <nghialee2301@gmail.com>
2024-01-28 03:03:45 +0700

    get emp from db

--- 01-thymeleaf-demo-employees-starter-list/sql-scripts/employee-directory.sql
+++ 01-thymeleaf-demo-employees-starter-list/sql-scripts/employee-directory.sql
@@ -20,9 +20,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
+	(1,'Nghia','Le','nghiale@luv2code.com'),
+	(2,'Thinh','Tran','thinhtran@luv2code.com'),
+	(3,'To','Le','tole@luv2code.com'),
+	(4,'Trung','Nguyen','trungnguyen@luv2code.com'),
+	(5,'Phat','Ho','phatho@luv2code.com');
 
